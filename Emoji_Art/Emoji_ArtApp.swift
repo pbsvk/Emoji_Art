a7d6d15644f28d97442235f9aecac01c670f6e71
Vamsi <bpadala@stevens.edu>
2024-11-06 15:38:44 -0500

    Initial Commit

---
+++ Emoji_Art/Emoji_ArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji_ArtApp.swift
+//  Emoji_Art
+//
+//  Created by Bhaskara Padala on 11/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Emoji_ArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Emoji_ArtTests/Emoji_ArtTests.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji_ArtTests.swift
+//  Emoji_ArtTests
+//
+//  Created by Bhaskara Padala on 11/6/24.
+//
+
+import Testing
+@testable import Emoji_Art
+
+struct Emoji_ArtTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
