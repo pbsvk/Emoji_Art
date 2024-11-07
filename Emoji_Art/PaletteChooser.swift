eabd745ec20bd5f5449463a6bdc8f07192d8d0e8
Vamsi <bpadala@stevens.edu>
2024-11-07 15:56:31 -0500

    emoji picking palette added

---
+++ Emoji_Art/PaletteChooser.swift
@@ -0,0 +1,18 @@
+//
+//  PaletteChooser.swift
+//  Emoji_Art
+//
+//  Created by Bhaskara Padala on 11/7/24.
+//
+
+import SwiftUI
+
+struct PaletteChooser: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PaletteChooser()
+}
